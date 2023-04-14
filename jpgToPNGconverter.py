d630def8d100097998458dae8483e2581f604545
BrindhaBarkavan <130645220+BrindhaBarkavan@users.noreply.github.com>
2023-04-14 20:53:17 +0530

    created JPG to PNG converter file
    This will get JPG images from one folder and convert it into PNG and saved those images into output folder

---
+++ jpgToPNGconverter.py
@@ -0,0 +1,19 @@
+import sys
+import os
+from PIL import Image
+from pygments.formatters import img
+
+# grab the first and second arguments from terminal
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+# check if folder exists , if not create it
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+# loop through the image folder convert to png ,save into output folder
+for filename in os.listdir(image_folder):
+    image = Image.open(f'{image_folder}{filename}')
+    clean_name = os.path.splitext(filename)[0]
+    image.save(f'{output_folder}{clean_name}.png', 'png')
+    print('All Done!')
